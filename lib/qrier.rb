5704c5091b226671a046340ddabe32890ee20280
Munir Uddin <munirchittagong@yahoo.com>
2022-11-26 21:18:37 +0000

    Initial Commit

---
+++ lib/qrier.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "qrier/version"
+
+module Qrier
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/qrier/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Qrier
+  VERSION = "0.1.0"
+end
